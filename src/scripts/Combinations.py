90c3cc5633dc6e67f3e0728afa743ffb58bfaa47
Fernando Perez Lara <fernando.perez.lara@outlook.com>
2020-11-21 15:06:34 +0100

    Functions to perform the combinations

---
+++ src/scripts/Combinations.py
@@ -0,0 +1,7 @@
+# This function will divide the combinations between a number of processes
+def distributeCombinations(listCombinations):
+    pass
+
+# This function will create a limited number of combinations
+def createCombination(powerPosition, listCombinations):
+    pass
