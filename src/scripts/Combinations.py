56cf2951b4bc4562a7b13ea2a81241276440613f
Fernando Perez Lara <fernando.perez.lara@outlook.com>
2020-11-22 15:04:18 +0100

    Distribution of combinations in processes

--- src/scripts/Combinations.py
+++ src/scripts/Combinations.py
@@ -1,6 +1,16 @@
+import multiprocessing
+from .Data import *
+
 # This function will divide the combinations between a number of processes
 def distributeCombinations(listCombinations):
-    pass
+    processes = [None]*lengthPowers
+
+    for i in range(0, lengthPowers):
+        processes[i] = multiprocessing.Process(target=createCombination, args=(i, listCombinations))
+        processes[i].start()
+
+    for i in range(0, lengthPowers):
+        processes[i].join()
 
 # This function will create a limited number of combinations
 def createCombination(powerPosition, listCombinations):
